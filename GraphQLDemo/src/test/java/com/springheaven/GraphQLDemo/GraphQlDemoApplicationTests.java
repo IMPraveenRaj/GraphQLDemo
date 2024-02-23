4e30718da6299420415f642d0a994c35b43a3478
IMPraveenRaj <impraveen13@gmail.com>
2024-02-23 10:22:22 +0100

    "Initial Commit for graphQL"

---
+++ GraphQLDemo/src/test/java/com/springheaven/GraphQLDemo/GraphQlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springheaven.GraphQLDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
